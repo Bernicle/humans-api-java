4218b91b97931aa9dd1d9b6aecf7232cc688fba1
Bernard Ponce <poncebernard123@gmail.com>
2023-12-31 18:19:51 +0800

    HTML Frontend Design Basics and Vanilla JavaScript RestAPI Client
    Generate two straightforward HTML files for Basic HTML Design for Frontend and Vanilla JavaScript Client for RestAPI Calls.

--- hr/src/main/java/com/pragmanila/hr/controller/HumanController.java
+++ hr/src/main/java/com/pragmanila/hr/controller/HumanController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -26,6 +27,7 @@ import com.pragmanila.hr.repository.HumanRepository;
 import com.pragmanila.hr.model.HumanResponseStructure;
 import com.pragmanila.hr.model.ResponseStructure;
 
+@CrossOrigin(origins = "*") //Allow Web Browser to Access the Restful API for Vanilla Javascript FrontEnd just for this Project only
 @RestController
 @RequestMapping("/api")
 public class HumanController {
